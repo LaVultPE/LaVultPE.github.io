d4a20f011104df72cd37343121577c4b2c4b6d4f
JurandirGuilherme <JurandirGuilherme1@gmail.com>
2020-04-26 15:36:51 -0300

    Re-criação do css, Adapitação para responcive

--- Js/main.js
+++ Js/main.js
@@ -3,5 +3,35 @@ $('.slider-principal').slick({
     infinite: true,
     speed: 300,
     slidesToShow: 1,
-    adaptiveHeight: true
+    adaptiveHeight: true,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
 });
